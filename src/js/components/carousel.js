795869338f6ea5f671c875be1f5a930e52fe54bf
idmitrov <mailto.dimitrov@gmail.com>
2016-03-12 16:08:01 +0200

    Implemented auto slide

--- src/js/components/carousel.js
+++ src/js/components/carousel.js
@@ -1,20 +1,69 @@
 $(function() {
     var carousel = {
-        slideSpeed: 2000,
+        /*
+         *  Carousel Settings:
+         *
+         *  @param {Number} autoSlideInterval
+         *  @param {String} carouselSelector
+         *  @param {String} slideSelector
+         *  @param {String} controlSelector
+         */
+        autoSlideInterval: 5000,
         carouselSelector: '.carousel',
         slideSelector: '.slide',
         controlSelector: '.control',
+        /*
+         *  Slide just once
+         */
         slide: function(slides, currentSlide) {
             slides.css('transform', 'translate3d(-'+ currentSlide * 100 +'%, 0, 0)');
         },
-        autoSlide: function() {
+        /*
+         *  Auto slide on interval
+         *
+         *  @param {jQuery Object} slides
+         *  @param {Number} currentSlide
+         *  @param {Number} slidesCount
+         */
+        autoSlide: function(slides, currentSlide, slidesCount) {
+            var direction = 'up';
 
+            setInterval(function() {
+                // If is last slide
+                if (currentSlide + 1 >= slidesCount) {
+                    direction = 'down';
+                }
+
+                // If is first slide
+                if (currentSlide - 1 < 0) {
+                    direction = 'up';
+                }
+
+                switch (direction) {
+                    case 'up':
+                        currentSlide += 1;
+                        break;
+                    case 'down':
+                        currentSlide -= 1;
+                        break;
+                    default: console.warn('Unknown sliding direction'); return;
+                }
+
+                carousel.slide(slides, currentSlide);
+            }, carousel.autoSlideInterval);
         },
-        bindEvents: function(slides, carouselControls, currentSlide) {
-            var slidesCount = slides.length || 0;
-
+        /*
+         *  Bind click event on Carousel control
+         *
+         *  @param {jQuery Object} slides
+         *  @param {jQuery Object} carouselControls
+         *  @param {Number} currentSlide
+         *  @param {Number} slidesCount
+         */
+        bindEvents: function(slides, carouselControls, currentSlide, slidesCount) {
             // Enable controls
             carouselControls.parent().addClass('enabled');
+
             // Attach click event
             carouselControls.on('click', function() {
                 var clickedControl = $(this);
@@ -38,29 +87,33 @@ $(function() {
                 }
             });
         },
+        /*
+         *  MAIN
+         */
         init: function() {
             var carouselNode = $(this.carouselSelector),
                 carouselControls = carouselNode.find(this.controlSelector),
                 slides = carouselNode.find(this.slideSelector),
                 currentSlide = 0,
-                direction = 'up';
+                slidesCount = slides.length || 0;
 
             // If there are slides and they are more than 1
             if (carouselNode && slides.length > 1) {
                 // If auto slide is ON
                 if (carouselNode.data('autoslide')) {
-                    this.autoSlide();
+                    this.autoSlide(slides, currentSlide, slidesCount);
                 }
 
                 // If controls are enabled
                 if (carouselNode.data('controls')) {
-                    this.bindEvents(slides, carouselControls, currentSlide);
+                    this.bindEvents(slides, carouselControls, currentSlide, slidesCount);
                 }
             }
         }
     };
 
     $(function() {
+        // On ready INIT MAIN
         carousel.init();
     });
 });

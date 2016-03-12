423eb201b447fca3154fa2f7fc66c3d797920254
idmitrov <mailto.dimitrov@gmail.com>
2016-03-12 16:23:53 +0200

    extract currentSlide as carousel global
    Reason: passing the variable to constructors cause the sync between controls/autoslide to be slower

--- src/js/components/carousel.js
+++ src/js/components/carousel.js
@@ -3,11 +3,13 @@ $(function() {
         /*
          *  Carousel Settings:
          *
+         *  @param {Number} currentSlide
          *  @param {Number} autoSlideInterval
          *  @param {String} carouselSelector
          *  @param {String} slideSelector
          *  @param {String} controlSelector
          */
+        currentSlide: 0,
         autoSlideInterval: 5000,
         carouselSelector: '.carousel',
         slideSelector: '.slide',
@@ -15,41 +17,40 @@ $(function() {
         /*
          *  Slide just once
          */
-        slide: function(slides, currentSlide) {
-            slides.css('transform', 'translate3d(-'+ currentSlide * 100 +'%, 0, 0)');
+        slide: function(slides) {
+            slides.css('transform', 'translate3d(-'+ carousel.currentSlide * 100 +'%, 0, 0)');
         },
         /*
          *  Auto slide on interval
          *
          *  @param {jQuery Object} slides
-         *  @param {Number} currentSlide
          *  @param {Number} slidesCount
          */
-        autoSlide: function(slides, currentSlide, slidesCount) {
+        autoSlide: function(slides, slidesCount) {
             var direction = 'up';
 
             setInterval(function() {
                 // If is last slide
-                if (currentSlide + 1 >= slidesCount) {
+                if (carousel.currentSlide + 1 >= slidesCount) {
                     direction = 'down';
                 }
 
                 // If is first slide
-                if (currentSlide - 1 < 0) {
+                if (carousel.currentSlide - 1 < 0) {
                     direction = 'up';
                 }
 
                 switch (direction) {
                     case 'up':
-                        currentSlide += 1;
+                        carousel.currentSlide += 1;
                         break;
                     case 'down':
-                        currentSlide -= 1;
+                        carousel.currentSlide -= 1;
                         break;
                     default: console.warn('Unknown sliding direction'); return;
                 }
 
-                carousel.slide(slides, currentSlide);
+                carousel.slide(slides);
             }, carousel.autoSlideInterval);
         },
         /*
@@ -57,10 +58,9 @@ $(function() {
          *
          *  @param {jQuery Object} slides
          *  @param {jQuery Object} carouselControls
-         *  @param {Number} currentSlide
          *  @param {Number} slidesCount
          */
-        bindEvents: function(slides, carouselControls, currentSlide, slidesCount) {
+        bindEvents: function(slides, carouselControls, slidesCount) {
             // Enable controls
             carouselControls.parent().addClass('enabled');
 
@@ -70,19 +70,19 @@ $(function() {
 
                 // Slide to the PREV
                 if (clickedControl.hasClass('prev')) {
-                    if (currentSlide - 1 >= 0) {
-                        currentSlide -= 1;
+                    if (carousel.currentSlide - 1 >= 0) {
+                        carousel.currentSlide -= 1;
 
-                        carousel.slide(slides, currentSlide);
+                        carousel.slide(slides);
                     }
                 }
 
                 // Slide to the NEXT
                 if (clickedControl.hasClass('next')) {
-                    if (currentSlide + 1 < slidesCount) {
-                        currentSlide += 1;
+                    if (carousel.currentSlide + 1 < slidesCount) {
+                        carousel.currentSlide += 1;
 
-                        carousel.slide(slides, currentSlide);
+                        carousel.slide(slides);
                     }
                 }
             });
@@ -94,19 +94,18 @@ $(function() {
             var carouselNode = $(this.carouselSelector),
                 carouselControls = carouselNode.find(this.controlSelector),
                 slides = carouselNode.find(this.slideSelector),
-                currentSlide = 0,
                 slidesCount = slides.length || 0;
 
             // If there are slides and they are more than 1
             if (carouselNode && slides.length > 1) {
                 // If auto slide is ON
                 if (carouselNode.data('autoslide')) {
-                    this.autoSlide(slides, currentSlide, slidesCount);
+                    this.autoSlide(slides, slidesCount);
                 }
 
                 // If controls are enabled
                 if (carouselNode.data('controls')) {
-                    this.bindEvents(slides, carouselControls, currentSlide, slidesCount);
+                    this.bindEvents(slides, carouselControls, slidesCount);
                 }
             }
         }
